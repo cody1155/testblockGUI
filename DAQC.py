e94407490b87a6406edde46e80e53b10bcd2757e
Cody <cody1155@gmail.com>
2019-08-19 00:47:24 -0700

    temperature adc

--- DAQC.py
+++ DAQC.py
@@ -3,20 +3,21 @@ import piplates.DAQCplate as DAQC
 
 #------INPUTS waiting for values
                                
-battery_volt_HIGH = 0
+battery_volt_HIGH = 11
 battery_volt_LOW = 0
 
-chamberPT_max = 0
-chamberPT_volt_HIGH = 0
+chamberPT_max = 500
+chamberPT_volt_HIGH = 5
 chamberPT_volt_LOW = 0
 
-thrustPT_max = 0
-thrustPT_volt_HIGH = 0
+thrustPT_max = 1500
+thrustPT_volt_HIGH = 5
 thrustPT_volt_LOW = 0
 
-temp_max = 0
-temp_volt_HIGH = 0
+temp_max = 257
+temp_volt_HIGH = 1.2
 temp_volt_LOW = 0
+offset = 0.5
 
 fileName = input("what would you like your file to be named?\n")
 fileName = str(fileName)
@@ -24,6 +25,8 @@ fileName = str(fileName)
 
 
 def adcFormula(data, max_value, volt_HIGH, volt_LOW, divisionFactor):
+    volt_range = volt_HIGH - volt_LOW
+    data = (max_value/volt_range) * (data-volt_LOW)
 
     #going to fill once components are chosen
 
@@ -38,7 +41,7 @@ def main():
         currentTime = time.strftime("%H:%M:%S")
 
         battery_raw = DAQC.getADC(1,0)
-        battery = adcFormula(battery_raw, 0, battery_volt_HIGH, battery_volt_LOW, 4)
+        battery = adcFormula(battery_raw, 1, battery_volt_HIGH, battery_volt_LOW, 4)
     
         chamberPT_raw = DAQC.getADC(1,1)
         chamberPT = adcFormula(chamberPT_raw, chamberPT_max, chamberPT_volt_HIGH, chamberPT_volt_LOW, 2)
@@ -47,10 +50,12 @@ def main():
         thrustPT = adcFormula(thrustPT_raw, thrustPT_max, thrustPT_volt_HIGH, thrustPT_volt_LOW, 2)
     
         temp_raw = DAQC.getADC(1,3)
-        temp = adcFormula(temp_raw, temp_max, temp_volt_HIGH, temp_volt_LOW, 2)
+        tempC = (temp_raw - offset) * 100
+        tempF = (tempC * 1.8) + 32
+        #temp = adcFormula(temp_raw, temp_max, temp_volt_HIGH, temp_volt_LOW, 2)
 
-        print('{}, {}, {}, {}, {}'.format(currentTime, battery, chamberPT, thrustPT, temp))
-        f.write('{}, {}, {}, {}, {}\n'.format(currentTime, battery, chamberPT, thrustPT, temp))
+        print('{}, {}, {}, {}, {}'.format(currentTime, battery, chamberPT, thrustPT, temp_raw))
+        f.write('{}, {}, {}, {}, {}\n'.format(currentTime, battery, chamberPT, thrustPT, tempF))
 
         time.sleep(.1)
         f.close()
